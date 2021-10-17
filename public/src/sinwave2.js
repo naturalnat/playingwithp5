70a8cfe61e7009396d3d4809aaa6170bbb42b4ea
Natalie Taktachev <ntaktach@gmail.com>
2021-10-17 13:25:01 -0400

    sinwave2 - b&w

--- public/src/sinwave2.js
+++ public/src/sinwave2.js
@@ -4,28 +4,28 @@ function setup() {
 }
 
 function draw(){
-    background(30)
+    background('white')
     rotateX(60)
     noFill()
-    stroke(255)
+    stroke(0, 30)
 
 
-    for (let i = 0; i < 50; i++){
-        let r = map(sin(frameCount / 2 ), -1, 1, 100, 200)
-        let g = map(i, 0, 50, 100, 200)
-        let b = map(cos(frameCount), -1, 1, 200, 100)
+    for (let i = 0; i < 70; i++){
+        // let r = map(sin(frameCount / 2 ), -1, 1, 100, 200)
+        // let g = map(i, 0, 50, 100, 200)
+        // let b = map(cos(frameCount), -1, 1, 200, 100)
 
-        stroke(r,g,b)
+        // stroke(r,g,b)
 
-        rotate(frameCount / 20)
+        rotate(frameCount / 40)
 
      
         beginShape()
-        for (let j = 0; j < 360; j += 60) {
+        for (let j = 0; j < 360; j += 10) {
             let rad = i * 5
             let x = rad * cos(j)
             let y = rad * sin(j)
-            let z = sin(frameCount * 2 + i * 10) * 50
+            let z = sin(frameCount * 2 + i * 20) * 50
 
             vertex(x, y, z)
         }
