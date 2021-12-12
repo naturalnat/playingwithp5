a56541f4a5cf36aabbb12bbac716b4a0e6c3e8e5
Natalie Taktachev <ntaktach@gmail.com>
2021-12-12 17:08:07 -0500

    rotate

--- public/src/sinwave3.js
+++ public/src/sinwave3.js
@@ -7,17 +7,21 @@ function setup() {
 }
 
 function draw() {
-    background(10, 20, 30) 
+    background(10, 20, 30)
     noFill()
     stroke(255)
 
     translate(width / 2, height / 2)
 
+    rotate(sin(frameCount + i))
     for (var i = 0; i < 200; i++) {
         push()
-    
-        rect (0, 0, 600 - i * 3, 600 - i * 3, 200 - i) 
+
+
+        rotate(sin(frameCount + i) * 100)
+        rect(0, 0, 600 - i * 3, 600 - i * 3, 200 - i)
+        //for every loop width and height should be decreased 
 
         pop()
-  }
+    }
 }
