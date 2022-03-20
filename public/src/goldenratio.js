a4bf63a5722ffe762303735fd813826a7bad7696
Natalie Taktachev <ntaktach@gmail.com>
2022-03-20 19:18:21 -0400

    golden ratio - set up canvas

---
+++ public/src/goldenratio.js
@@ -0,0 +1,11 @@
+function setup() { 
+    createCanvas(windowWidth, windowHeight); 
+    colorMode(RGB, 1);
+    noStroke();
+}
+
+function draw(){
+    scale(width, height)
+    background(0);
+    fill(1);
+}
