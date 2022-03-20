482649a851dbdcf89a9d932c417cfeee072f785c
Natalie Taktachev <ntaktach@gmail.com>
2022-03-20 19:29:25 -0400

    draw circle with polar coordinates

--- public/src/goldenratio.js
+++ public/src/goldenratio.js
@@ -4,8 +4,16 @@ function setup() {
     noStroke();
 }
 
-function draw(){
-    scale(width, height)
+function draw() {
+    scale(width, height);
     background(0);
     fill(1);
+
+    //polar coordinates - relational in reference to location(pole)
+    const a = 0.5;
+    const radius = 0.1;
+    const x = 0.5 + cos(a * 3.14) * radius;
+    const y = 0.5 + sin(a * 3.14) * radius; 
+    const r = 0.01; 
+    circle(x,y,r);
 }
