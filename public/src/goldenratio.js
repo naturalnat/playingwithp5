c75a8e53721fd9d670c5294c53548188f44b3ba3
Natalie Taktachev <ntaktach@gmail.com>
2022-03-20 19:35:50 -0400

    more circles

--- public/src/goldenratio.js
+++ public/src/goldenratio.js
@@ -1,8 +1,11 @@
 function setup() { 
-    createCanvas(windowWidth, windowHeight); 
+    const size = min(windowWidth, windowHeight)
+    createCanvas(size, size); 
     colorMode(RGB, 1);
     noStroke();
 }
+
+
 
 function draw() {
     scale(width, height);
@@ -10,10 +13,14 @@ function draw() {
     fill(1);
 
     //polar coordinates - relational in reference to location(pole)
-    const a = 0.5;
-    const radius = 0.1;
-    const x = 0.5 + cos(a * 3.14) * radius;
-    const y = 0.5 + sin(a * 3.14) * radius; 
-    const r = 0.01; 
-    circle(x,y,r);
+    const count = 100;
+    for (let i = 0; i < count; i++){
+        const radius = 0.25;
+        const f = i / count;
+        const a = f;
+        const x = 0.5 + cos(a * TWO_PI) * radius;
+        const y = 0.5 + sin(a * TWO_PI) * radius; 
+        const r = 0.01; 
+        circle(x,y,r);
+    }
 }
