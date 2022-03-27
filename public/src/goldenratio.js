d6e46658bf4e9ba6cf3ca58c3e62b657efeb4407
Natalie Taktachev <ntaktach@gmail.com>
2022-03-27 18:30:26 -0400

    zoom out effect

--- public/src/goldenratio.js
+++ public/src/goldenratio.js
@@ -11,18 +11,24 @@ const radius = Math.sqrt(0.5);
 const PHI = (1 + Math.sqrt(5)) / 2 //phi = "the golden ratio", most irrational number 
 const dotSize = 0.05;
 
+let t;
+const frames = 1000; 
+
 function draw() {
+    t = fract(frameCount / frames); //stays btwn 0-1 when looping and loops forever
+   
     scale(width, height);
     background(0);
     fill(1);
 
     //polar coordinates - relational in reference to location(pole)
    
-    const count = 1000;
+    const count = 1000 * t; //zoom out effect 
     for (let i = 0; i < count; i++){
         const f = i / count;
-        const a = i * PHI;
+        const a = i * PHI; 
         const dist  = f * radius; 
+
         const x = 0.5 + cos(a * TWO_PI) * dist;
         const y = 0.5 + sin(a * TWO_PI) * dist; 
        
