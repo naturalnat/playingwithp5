ac5fd689c989b85037ca4dc8de43d1d8605d8c25
Natalie Taktachev <ntaktach@gmail.com>
2022-04-03 16:12:35 -0400

    play song on canvas press

--- public/src/audiovisualizer.js
+++ public/src/audiovisualizer.js
@@ -1,7 +1,29 @@
+let song 
+
+function preload() {
+    song = loadSound('/src/Ochi_Chernye.mp3')
+}
+
 function setup() { 
     createCanvas(windowWidth, windowHeight); 
+
 }
 
 function draw(){
-    background(0)
+    background(0);
 }
+
+function mouseClicked() {
+    if (song.isPlaying()) {
+        song.pause()
+    } else { 
+        song.play()
+    }
+}
+
+
+// function canvasPressed() {
+//     // playing a sound file on a user gesture
+//     // is equivalent to `userStartAudio()`
+//     song.play();
+//   }
