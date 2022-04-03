d32041ebc05e5d320ae0cae4409bcbccc729ec75
Natalie Taktachev <ntaktach@gmail.com>
2022-04-03 16:27:48 -0400

    connect points with line

--- public/src/audiovisualizer.js
+++ public/src/audiovisualizer.js
@@ -14,19 +14,22 @@ function setup() {
 function draw(){
     background(0);
     stroke(225);
+    noFill();
 
     const wave = fft.waveform()
     //store waveform data 
     //waveform method returns array w elements
 
+    beginShape() //connect points with line
     for (let i = 0; i < width; i++){ //loop thru waveform data to create wave across canvas 
         let index = floor(map(i, 0, width, 0, wave.length)) //value of index must be int so has to be floored; need to map to create wave
     
         const x = i //for loop variable 
         const y = wave[index] * 300 + height / 2 // waveform value at current index; waveform val is btwn -1 and 1 so make bigger 
         //offset height to middle of canvas 
-        point(x, y)
+        vertex(x, y)
     }
+    endShape() 
 }
 
 function mouseClicked() {
